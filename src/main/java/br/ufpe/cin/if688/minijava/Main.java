49de565ce5e81e7ccbd5a293052f82f74bb162d2
rodrigoap2 <rap2@cin.ufpe.br>
2019-10-28 16:50:59 -0300

    Modificação do main e criação do zip com os arquivos finais

--- src/main/java/br/ufpe/cin/if688/minijava/Main.java
+++ src/main/java/br/ufpe/cin/if688/minijava/Main.java
@@ -9,6 +9,7 @@ import br.ufpe.cin.if688.minijava.ast.*;
 import br.ufpe.cin.if688.minijava.ast.Program;
 import br.ufpe.cin.if688.minijava.visitor.MiniJavaVisitor;
 import br.ufpe.cin.if688.minijava.visitor.PrettyPrintVisitor;
+import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import java.io.IOException;
@@ -17,51 +18,17 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException{
+        CharStream charStream = CharStreams.fromStream(System.in);
+        MiniJavaGrammarLexer lexer = new MiniJavaGrammarLexer(charStream);
 
-        MainClass main = new MainClass(
-                new Identifier("Teste"),
-                new Identifier("Testando"),
-                new Print(new IntegerLiteral(2))
-        );
+        CommonTokenStream commonTokenStream = new CommonTokenStream(lexer);
+        MiniJavaGrammarParser parser = new MiniJavaGrammarParser(commonTokenStream);
 
-        VarDeclList vdl1 = new VarDeclList();
-        vdl1.addElement(new VarDecl(
-                new BooleanType(),
-                new Identifier("flag")
-        ));
-        vdl1.addElement(new VarDecl(
-                new IntegerType(),
-                new Identifier("num")
-        ));
+        MiniJavaVisitor miniJava = new MiniJavaVisitor();
+        Program program = (Program) miniJava.visit(parser.goal());
 
-        MethodDeclList mdl = new MethodDeclList();
-
-        ClassDeclSimple A = new ClassDeclSimple(
-                new Identifier("A"), vdl1, mdl
-        );
-
-        ClassDeclExtends B = new ClassDeclExtends(
-                new Identifier("B"), new Identifier("A"),
-                new VarDeclList(), new MethodDeclList()
-        );
-
-        VarDeclList vdl2 = new VarDeclList();
-        vdl2.addElement(new VarDecl(
-                new IdentifierType("A"),
-                new Identifier("obj")
-        ));
-        ClassDeclSimple C = new ClassDeclSimple(
-                new Identifier("C"), vdl2, new MethodDeclList()
-        );
-
-        ClassDeclList cdl = new ClassDeclList();
-        cdl.addElement(A);
-        cdl.addElement(B);
-        cdl.addElement(C);
-
-        Program p = new Program(main, cdl);
-        PrettyPrintVisitor ppv = new PrettyPrintVisitor();
-        ppv.visit(p);
+        PrettyPrintVisitor printVisitor = new PrettyPrintVisitor();
+        printVisitor.visit(program);
     }
 
 }
